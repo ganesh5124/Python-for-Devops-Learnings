4a249a3c3053abcd8fdf57544e090558038a9b7a
Ganesh5124 <pvgkumar2001@gmail.com>
2024-11-04 19:06:07 +0530

    added some practice

---
+++ Day-1/main.py
@@ -0,0 +1,28 @@
+# List Declaration
+
+# In my_list contains collection numeric items
+my_list = [3, 9, 4, 5, 10, 6]
+
+# Contains collection string items
+mystring_list = ["a", "b" , "c"] 
+
+# Contains collection mixed items
+mixed_list = ["a", 6 , "c", True, 4, False]  
+
+print(mixed_list[-1])
+
+print(my_list[1:4])
+
+
+
+
+
+
+
+
+
+
+# Functions
+# my_list.append(6)
+# print(my_list.count(6))
+# print(my_list)
